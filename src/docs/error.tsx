67fdb14fc8ded29ea0e4f3c6234c86b7b3412275
roggc <roggc9@gmail.com>
2026-07-12 01:24:26 +0200

    error page

--- src/docs/error.tsx
+++ src/docs/error.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-export default function Page({ error: { message, stack } }: { error: Error }) {
+export default function Page({ error }: { error: Error }) {
   return (
     <main className="flex-1 flex flex-col items-center justify-center p-4">
       <div className="max-w-md w-full text-center space-y-6">
         <h1 className="text-3xl font-bold text-red-600">Error</h1>
         <p className="text-lg text-gray-700">
-          An unexpected error has occurred baby. Please try again later.
+          An unexpected error has occurred. Please try again later.
         </p>
         <a
           href="/"
@@ -15,10 +15,10 @@ export default function Page({ error: { message, stack } }: { error: Error }) {
           Go to Home
         </a>
       </div>
-      <div className="mt-6 text-sm text-gray-500">
+      {/* <div className="mt-6 text-sm text-gray-500">
         <pre className="whitespace-pre-wrap break-words">{message}</pre>
         <pre className="whitespace-pre-wrap break-words">{stack}</pre>
-      </div>
+      </div> */}
     </main>
   );
 }
